0023b1656155a55c3df08b088672cf39ed6e10d6
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-01 18:00:36 +0530

    use morgan third-party middlewere

--- index.js
+++ index.js
@@ -53,40 +53,80 @@
 // })
 
 // How to display html files
+// const express = require("express");
+// const app = express();
+
+// app.set("view engine","ejs");
+
+// app.use("/",(req,res,next)=>{
+//     console.log("This is a Middlewere");
+//     let a = 5;
+//     let b = 6;
+//     console.log("a + b:",a+b);
+//     return next();
+// })
+// // create 4 four routes
+// app.get("/",(req ,res)=>{
+//     console.log("Home Page");
+//     // res.send("Home Pages...!!");
+//     res.render("index")
+// })
+
+// app.get("/about",(req ,res)=>{
+//     console.log("about Page");
+//     res.send("about Pages...!!");
+// })
+
+// app.get("/profiles",(req ,res)=>{
+//     console.log("profiles Page");
+//     res.send("profiles Pages...!!");
+// })
+
+// app.get("/contact",(req ,res)=>{
+//     console.log("contact Page");
+//     res.send("contact Pages...!!");
+// })
+
+// app.listen(3000,(req,res)=>{
+//     console.log(`Server is running on the port ${3000}`)
+// });
+
+
+// built in middlewere
+// example-> morgan ->it is http request logger middlewere in nodejs
+
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
 
 app.set("view engine","ejs");
 
-app.use("/",(req,res,next)=>{
-    console.log("This is a Middlewere");
+app.use((req,res,next)=>{
+    console.log("Morgan middlewere");
     let a = 5;
     let b = 6;
-    console.log("a + b:",a+b);
-    return next();
+    console.log("a+b:",a+b);
+    next();
 })
-// create 4 four routes
-app.get("/",(req ,res)=>{
-    console.log("Home Page");
-    // res.send("Home Pages...!!");
+
+// routes
+app.get("/",(req,res)=>{
+    console.log("Home Pages");
     res.render("index")
+});
+
+app.get("/about",(req,res)=>{
+    console.log("about pages");
+    res.send("about pages..!!");
+});
+
+app.get("/contact",(req,res)=>{
+    console.log("contact pages..!!!");
+    res.send("contact pages..!!!");
 })
 
-app.get("/about",(req ,res)=>{
-    console.log("about Page");
-    res.send("about Pages...!!");
-})
-
-app.get("/profiles",(req ,res)=>{
-    console.log("profiles Page");
-    res.send("profiles Pages...!!");
-})
-
-app.get("/contact",(req ,res)=>{
-    console.log("contact Page");
-    res.send("contact Pages...!!");
-})
-
-app.listen(3000,(req,res)=>{
-    console.log(`Server is running on the port ${3000}`)
+app.listen(3000,(req ,res)=>{
+    console.log(`server is running on the port:${3000}`)
 })
