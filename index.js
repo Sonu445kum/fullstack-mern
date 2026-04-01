d7ba496c4f9d246ddcde212d74a4ba9e70df8bba
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-01 16:34:02 +0530

    write 3 routes

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // Create a simple server
-const http = require("http");
-const server =http.createServer((req,res)=>{
-    res.end("Hello World..!!!");
-})
+// const http = require("http");
+// const server =http.createServer((req,res)=>{
+//     res.end("Hello World..!!!");
+// })
 
-server.listen(3000);
+// server.listen(3000);
+
+// create a simple routes
+// const http = require("http");
+// const server =http.createServer((req,res)=>{
+//     // routes
+//     if(req.url == "/about"){
+//         console.log("About Pages...!!"); // its display on the terminal
+//         res.end("About Pages") // its display on the webpages
+//     }
+//     else if(req.url == "/profile"){
+//         console.log("Profiles pages");
+//         res.end("profiles")
+//     }else if(req.url === "/"){
+//         console.log("Home Pages...!!!");
+//         res.end("Home Pages..!!")
+//     }
+
+//     // res.end("Hello sonu");
+// });
+
+// server.listen(3000);
